ae4c41e0090a907ddf960b980adab5ddbd2ddc59
a-sambhab <saisambhav21chaini@gmail.com>
2021-09-12 01:09:50 +0530

    minor edits

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -6,7 +6,7 @@ export default function Landing() {
         <>
         <div className="bg-color-land">
             <div className="title" >DINOCARE </div>
-            <div className="dino-bg"> <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1631305497/DINOCARE/dinosaur-icon-2_j1k6bw.png" className="dino-bg-size" srcset="" /> </div>
+            <div className="dino-bg"> <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1631305497/DINOCARE/dinosaur-icon-2_j1k6bw.png" className="dino-bg-size" srcset="" alt="landingdino"/> </div>
         </div>
         </>
     )
