8d0391e0b97d514f9e595f2a6839d5d4ebc84167
Neeraj2002 <nnr0702e@gmail.com>
2021-09-11 23:58:55 +0530

    Updated Navbar, Landing, About Page

---
+++ src/components/Navbar/Menuitems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Dinos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+
+]
