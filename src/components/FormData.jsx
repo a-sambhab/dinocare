deed56a97aa3cea29fce08e03816ebe326522076
a-sambhab <saisambhav21chaini@gmail.com>
2021-09-12 14:24:34 +0530

    Knowmoredata

---
+++ src/components/FormData.jsx
@@ -0,0 +1,58 @@
+const FormData = [
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+    {
+        question:"What is the thing you’d never say to another person?",
+        option1:"Comment on the way the look",
+        option2:"Comment on their personal lives",
+        option3:"Comment on their career decisions",
+        option4:"Comment on their behaviour",
+        option5:"Swear at them"
+    },
+]
